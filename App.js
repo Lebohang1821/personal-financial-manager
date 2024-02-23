cb765f21544aacdab04f256ef6eeb85f533024c4
Lebohang1821 <millboylebohang@gmail.com>
2024-02-23 15:57:09 +0200

    app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>personal-financial-manager</Text>
       <StatusBar style="auto" />
     </View>
   );
