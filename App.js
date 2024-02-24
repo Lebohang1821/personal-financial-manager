6aa70cc7f8a9eea2cf762b58bfc4e65243cb4697
Motekema <motekema@gmail.com>
2024-02-24 16:28:24 +0200

    Added

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import { Text, Platform, View } from "react-native";
-import { Home, Portfolio, Settings, Transaction } from "./screens";
-import Profile from "./screens/Profile";
-import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { Text, View, Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Entypo } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
+import {
+  Entypo,
+  MaterialIcons,
+  FontAwesome,
+  Ionicons,
+} from "@expo/vector-icons";
+import Home from "./screens/Home";
+import Portfolio from "./screens/Portfolio";
+import Transaction from "./screens/Transaction";
+import Profile from "./screens/Profile";
+import Settings from "./screens/Settings";
+import { NavigationContainer } from "@react-navigation/native";
 
-// Thanks for watching
 const Tab = createBottomTabNavigator();
+
 const screenOptions = {
   tabBarShowLabel: false,
   headerShown: false,
@@ -23,6 +29,7 @@ const screenOptions = {
     background: "#fff",
   },
 };
+
 export default function App() {
   return (
     <NavigationContainer>
@@ -31,105 +38,85 @@ export default function App() {
           name="Home"
           component={Home}
           options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <View
-                  style={{ alignItems: "center", justifyContent: "center" }}
-                >
-                  <Entypo
-                    name="home"
-                    size={24}
-                    color={focused ? "#16247d" : "#111"}
-                  />
-                  <Text style={{ fonSize: 12, color: "#16247d" }}>HOME</Text>
-                </View>
-              );
-            },
+            tabBarIcon: ({ focused }) => (
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <Entypo
+                  name="home"
+                  size={24}
+                  color={focused ? "#ff0000" : "#111"}
+                />
+                <Text style={{ fontSize: 12, color: "#ff0000" }}>HOME</Text>
+              </View>
+            ),
           }}
         />
         <Tab.Screen
           name="Portfolio"
           component={Portfolio}
           options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <View
-                  style={{ alignItems: "center", justifyContent: "center" }}
-                >
-                  <Entypo
-                    name="wallet"
-                    size={24}
-                    color={focused ? "#16247d" : "#111"}
-                  />
-                  <Text style={{ fonSize: 12, color: "#16247d" }}>WALLET</Text>
-                </View>
-              );
-            },
+            tabBarIcon: ({ focused }) => (
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <Entypo
+                  name="wallet"
+                  size={24}
+                  color={focused ? "#ff0000" : "#111"}
+                />
+                <Text style={{ fontSize: 12, color: "#ff0000" }}>WALLET</Text>
+              </View>
+            ),
           }}
         />
         <Tab.Screen
           name="Transaction"
           component={Transaction}
           options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <View
-                  style={{
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "#16247d",
-                    width: Platform.OS == "ios" ? 50 : 60,
-                    height: Platform.OS == "ios" ? 50 : 60,
-                    top: Platform.OS == "ios" ? -10 : -20,
-                    borderRadius: Platform.OS == "ios" ? 25 : 30,
-                  }}
-                >
-                  <FontAwesome name="exchange" size={24} color="#fff" />
-                </View>
-              );
-            },
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: focused ? "#ff0000" : "#111",
+                  width: Platform.OS == "ios" ? 50 : 60,
+                  height: Platform.OS == "ios" ? 50 : 60,
+                  top: Platform.OS == "ios" ? -10 : -20,
+                  borderRadius: Platform.OS == "ios" ? 25 : 30,
+                }}
+              >
+                <FontAwesome name="exchange" size={24} color="#fff" />
+              </View>
+            ),
           }}
         />
         <Tab.Screen
           name="Profile"
           component={Profile}
           options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <View
-                  style={{ alignItems: "center", justifyContent: "center" }}
-                >
-                  <MaterialIcons
-                    name={focused ? "person" : "person-outline"}
-                    size={24}
-                    color={focused ? "#16247d" : "#111"}
-                  />
-                  <Text style={{ fonSize: 12, color: "#16247d" }}>PROFILE</Text>
-                </View>
-              );
-            },
+            tabBarIcon: ({ focused }) => (
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <MaterialIcons
+                  name="person"
+                  size={24}
+                  color={focused ? "#ff0000" : "#111"}
+                />
+                <Text style={{ fontSize: 12, color: "#ff0000" }}>PROFILE</Text>
+              </View>
+            ),
           }}
         />
         <Tab.Screen
           name="Settings"
           component={Settings}
           options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <View
-                  style={{ alignItems: "center", justifyContent: "center" }}
-                >
-                  <Ionicons
-                    name="settings"
-                    size={24}
-                    color={focused ? "#16247d" : "#111"}
-                  />
-                  <Text style={{ fonSize: 12, color: "#16247d" }}>
-                    SETTINGS
-                  </Text>
-                </View>
-              );
-            },
+            tabBarIcon: ({ focused }) => (
+              <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <Ionicons
+                  name="settings"
+                  size={24}
+                  color={focused ? "#ff0000" : "#111"}
+                />
+                <Text style={{ fontSize: 12, color: "#ff0000" }}>SETTINGS</Text>
+              </View>
+            ),
           }}
         />
       </Tab.Navigator>
