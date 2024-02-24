7200dabc4c5519a6d6703135a67f84e6b111364f
Motekema <motekema@gmail.com>
2024-02-24 17:22:20 +0200

    Added

--- screens/Profile.js
+++ screens/Profile.js
@@ -162,7 +162,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
     paddingHorizontal: 20,
-    paddingTop: 40,
+    paddingTop: 50,
     backgroundColor: "#f9f9f9",
   },
   imageContainer: {
@@ -186,7 +186,7 @@ const styles = StyleSheet.create({
   },
   label: {
     width: 120,
-    marginRight: 10,
+    marginRight: 5,
     fontWeight: "bold",
   },
   text: {
