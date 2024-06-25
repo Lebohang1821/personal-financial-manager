a910d886b9140ac8f5dabc8d5ff6fe2f812720fa
Motekema <motekema@gmail.com>
2024-06-25 16:57:47 +0200

    Added

--- frontend/SRC/Components/Btn.js
+++ frontend/SRC/Components/Btn.js
@@ -9,7 +9,7 @@ export default function Btn({ bgColor, btnLabel, textColor, Press }) {
         backgroundColor: bgColor,
         borderRadius: 100,
         alignItems: "center",
-        width: 350,
+        width: 300,
         paddingVertical: 5,
         marginVertical: 10,
       }}
