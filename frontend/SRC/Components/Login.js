13273eb76a84b6dd516f52382fc556bf2b4f2cbc
Lebohang1821 <millboylebohang@gmail.com>
2024-06-25 12:17:37 +0200

    login

--- frontend/SRC/Components/Login.js
+++ frontend/SRC/Components/Login.js
@@ -1,89 +1,91 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Dimensions } from "react-native";
 import Background from "./Background";
 import Btn from "./Btn";
 import { darkGreen } from "./Constants";
 import Field from "./Field";
 
 const Login = (props) => {
+  const screenWidth = Dimensions.get("window").width;
+  const containerWidth = screenWidth > 460 ? 460 : screenWidth;
+
   return (
     <Background>
-      <View style={{ alignItems: "center", width: 460 }}>
-        <Text
-          style={{
-            color: "white",
-            fontSize: 64,
-            fontWeight: "bold",
-            marginVertical: 20,
-          }}
-        >
-          Login
-        </Text>
-        <View
-          style={{
-            backgroundColor: "transparent",
-            height: 700,
-            width: 460,
-            borderTopLeftRadius: 130,
-            paddingTop: 100,
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ fontSize: 40, color: "darkgoldenrod", fontWeight: "bold" }}>
-            Welcome Back
-          </Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View style={{ alignItems: "center", width: containerWidth }}>
           <Text
             style={{
-              color: "grey",
-              fontSize: 19,
+              color: "white",
+              fontSize: 64,
               fontWeight: "bold",
-              marginBottom: 20,
+              marginVertical: 20,
+              textAlign: "center",
             }}
           >
-            Login to your account
+            Login
           </Text>
-          <Field
-            placeholder="Email / Username"
-            keyboardType={"email-address"}
-          />
-          <Field placeholder="Password" secureTextEntry={true} />
           <View
             style={{
-              alignItems: "flex-end",
-              width: "78%",
-              paddingRight: 16,
-              marginBottom: 200,
+              backgroundColor: "transparent",
+              width: containerWidth,
+              borderTopLeftRadius: 130,
+              paddingTop: 100,
+              alignItems: "center",
             }}
           >
-            <Text
-              style={{ color: "silver", fontWeight: "bold", fontSize: 16 }}
-            >
-              Forgot Password ?
+            <Text style={{ fontSize: 40, color: "darkgoldenrod", fontWeight: "bold", textAlign: "center" }}>
+              Welcome Back
             </Text>
+            <Text
+              style={{
+                color: "grey",
+                fontSize: 19,
+                fontWeight: "bold",
+                marginBottom: 20,
+                textAlign: "center",
+              }}
+            >
+              Login to your account
+            </Text>
+            <Field
+              placeholder="Email / Username"
+              keyboardType={"email-address"}
+              containerStyle={{ width: "78%" }}
+            />
+            <Field
+              placeholder="Password"
+              secureTextEntry={true}
+              containerStyle={{ width: "78%", marginBottom: 20 }}
+            />
+            <TouchableOpacity
+              style={{ alignSelf: "flex-end", marginRight: 16, marginBottom: 20 }}
+              onPress={() => {
+                // Implement your "Forgot Password" functionality here
+              }}
+            >
+              <Text style={{ color: "silver", fontWeight: "bold", fontSize: 16 }}>
+                Forgot Password ?
+              </Text>
+            </TouchableOpacity>
+            <Btn
+              textColor="white"
+              bgColor="blue"
+              btnLabel="Login"
+              Press={() => {
+                props.navigation.navigate("Main");
+              }}
+              containerStyle={{ marginBottom: 20, width: "78%" }}
+            />
           </View>
-          <Btn
-            textColor="white"
-            bgColor="blue"
-            btnLabel="Login"
-            Press={() => {
-              props.navigation.navigate("Main");
-            }}
-          />
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <Text style={{ color: darkGreen, fontSize: 16, fontWeight: "bold" }}>
-              Don't have an account ?{" "}
+          <View style={{ alignItems: "center", marginTop: 20 }}>
+            <Text style={{ color: darkGreen, fontSize: 16, fontWeight: "bold", textAlign: "center", marginBottom: 10 }}>
+              Don't have an account?
             </Text>
             <TouchableOpacity
               onPress={() => props.navigation.navigate("Signup")}
             >
               <Text
-                style={{ color: "white", fontWeight: "bold", fontSize: 16 }}
+                style={{ color: "white", fontWeight: "bold", fontSize: 16, textAlign: "center" }}
               >
                 Signup
               </Text>
