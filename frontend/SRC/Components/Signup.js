a9c26ed352f5172551b2a9bbb27f4ed7334bfea5
Lebohang1821 <millboylebohang@gmail.com>
2024-06-25 12:22:55 +0200

    signup

--- frontend/SRC/Components/Signup.js
+++ frontend/SRC/Components/Signup.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Dimensions } from "react-native";
 import Background from "./Background";
 import Btn from "./Btn";
 import { darkGreen } from "./Constants";
 import Field from "./Field";
 
 const Signup = (props) => {
+  const screenWidth = Dimensions.get("window").width;
+  const containerWidth = screenWidth > 460 ? 460 : screenWidth;
+
   return (
     <Background>
-      <View style={{ alignItems: "center", width: 460 }}>
+      <View style={{ alignItems: "center", width: containerWidth }}>
         <Text
           style={{
             color: "white",
             fontSize: 64,
             fontWeight: "bold",
             marginTop: 20,
+            textAlign: "center",
           }}
         >
           Register
@@ -25,6 +29,7 @@ const Signup = (props) => {
             fontSize: 19,
             fontWeight: "bold",
             marginBottom: 20,
+            textAlign: "center",
           }}
         >
           Create a new account
@@ -32,54 +37,46 @@ const Signup = (props) => {
         <View
           style={{
             backgroundColor: "transparent",
-            height: 700,
-            width: 460,
+            width: containerWidth,
             borderTopLeftRadius: 130,
             paddingTop: 50,
+            paddingBottom: 20,
             alignItems: "center",
           }}
         >
-          <Field placeholder="First Name" />
-          <Field placeholder="Last Name" />
+          <Field placeholder="First Name" containerStyle={{ marginBottom: 10, width: "78%" }} />
+          <Field placeholder="Last Name" containerStyle={{ marginBottom: 10, width: "78%" }} />
           <Field
             placeholder="Email / Username"
             keyboardType={"email-address"}
+            containerStyle={{ marginBottom: 10, width: "78%" }}
           />
-          <Field placeholder="Contact Number" keyboardType={"number"} />
-          <Field placeholder="Password" secureTextEntry={true} />
-          <Field placeholder="Confirm Password" secureTextEntry={true} />
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              width: "78%",
-              paddingRight: 16,
-            }}
-          >
-            <Text style={{ color: "grey", fontSize: 16 }}>
+          <Field
+            placeholder="Contact Number"
+            keyboardType={"number-pad"}
+            containerStyle={{ marginBottom: 10, width: "78%" }}
+          />
+          <Field
+            placeholder="Password"
+            secureTextEntry={true}
+            containerStyle={{ marginBottom: 10, width: "78%" }}
+          />
+          <Field
+            placeholder="Confirm Password"
+            secureTextEntry={true}
+            containerStyle={{ marginBottom: 10, width: "78%" }}
+          />
+          <View style={{ flexDirection: "row", alignItems: "center", width: "78%", marginBottom: 10 }}>
+            <Text style={{ color: "grey" }}>
               By signing in, you agree to our{" "}
             </Text>
-            <Text
-              style={{ color: darkGreen, fontWeight: "bold", fontSize: 16 }}
-            >
+            <Text style={{ color: darkGreen, fontWeight: "bold" }}>
               Terms & Conditions
             </Text>
           </View>
-
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              width: "78%",
-              paddingRight: 16,
-              marginBottom: 10,
-            }}
-          >
-            <Text style={{ color: "grey", fontSize: 16 }}>and </Text>
-            <Text
-              style={{ color: darkGreen, fontWeight: "bold", fontSize: 16 }}
-            >
+          <View style={{ flexDirection: "row", alignItems: "center", width: "78%", marginBottom: 10 }}>
+            <Text style={{ color: "grey" }}>and </Text>
+            <Text style={{ color: darkGreen, fontWeight: "bold" }}>
               Privacy Policy
             </Text>
           </View>
@@ -90,23 +87,16 @@ const Signup = (props) => {
             Press={() => {
               props.navigation.navigate("Main");
             }}
+            containerStyle={{ marginBottom: 10, width: "78%" }}
           />
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
             <Text style={{ color: "lightgrey", fontSize: 16, fontWeight: "bold" }}>
-              Already have an account ?{" "}
+              Already have an account?{" "}
             </Text>
             <TouchableOpacity
               onPress={() => props.navigation.navigate("Login")}
             >
-              <Text
-                style={{ color: darkGreen, fontWeight: "bold", fontSize: 16 }}
-              >
+              <Text style={{ color: darkGreen, fontSize: 16, fontWeight: "bold" }}>
                 Login
               </Text>
             </TouchableOpacity>
